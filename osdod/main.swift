25309621e53e1ab5921992d424aac8c1d1f22362
Vlad Yarotsky <vladimir.yarotsky@gmail.com>
2018-10-05 18:12:54 -0700

    Initial Commit

---
+++ osdod/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  osdod
+//
+//  Created by Vlad Yarotsky on 10/5/18.
+//  Copyright © 2018 Vlad Yarotsky. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
